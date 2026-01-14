fd4ea5c760ae50611b133d666dac6d57b149990a
bohn13 <bod3171@gmail.com>
2026-01-14 13:54:12 +0100

    Fix flake8 x2

--- app/main.py
+++ app/main.py
@@ -8,12 +8,14 @@ def cache(func: Callable) -> Callable:
     @wraps(func)
     def wrapper(*args, **kwargs):
         key = (args, tuple(sorted(kwargs.items())))
+
         if key in cached_results:
             print("Getting from cache")
             return cached_results[key]
-        else:
-            print("Calculating new result")
-            result = func(*args, **kwargs)
-            cached_results[key] = result
-            return result
+
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        cached_results[key] = result
+        return result
+
     return wrapper
