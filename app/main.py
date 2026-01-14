b2d822890883b702a9f7790554327110192fb42b
bohn13 <bod3171@gmail.com>
2026-01-14 14:03:03 +0100

    Fix flake8 x4

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 from functools import wraps
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     cached_results = {}
 
     @wraps(func)
-    def wrapper(*args, **kwargs):
+    def wrapper(*args, **kwargs) -> Any:
         key = (args, tuple(sorted(kwargs.items())))
 
         if key in cached_results:
