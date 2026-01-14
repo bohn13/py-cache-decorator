a293a089b8dfbaf3faf55a12f2222c5e51f92c1f
bohn13 <bod3171@gmail.com>
2026-01-14 13:48:58 +0100

    Fix flake8

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from typing import Callable
 from functools import wraps
 
+
 def cache(func: Callable) -> Callable:
     cached_results = {}
+
     @wraps(func)
     def wrapper(*args, **kwargs):
         key = (args, tuple(sorted(kwargs.items())))
